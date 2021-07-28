6e95a08f473e5f5ec0e2844999be5465460ce31e
ganeshsugavanam <sganesh456@gmail.com>
2021-07-28 07:38:45 +0530

    Added visual tests and guidelines in readme

--- README.md
+++ README.md
@@ -6,15 +6,32 @@ A test project using the automation tool, [Cypress](https://www.cypress.io/). Th
 - Verification of Bill pay functionality
 - Verification of Account services section
 - Verification of Balance and transaction table details
+- Verification of Login, Create Account, Account Overview, Bill Pay and Transaction API's
+- Verification of different error codes from above api's
+- Visual tests for Account overview page before and after a billpay
 
 ## Requires
-1. [node](https://nodejs.org/en/)
+1. [node](https://nodejs.org/en/) - Latest version
 1. [git](https://git-scm.com/)
 
 ## Setup
 1. clone: `https://github.com/sganesh456/test-assignments.git`
 1. install: `npm i`
+1. set-percy-token: 
+In the shell window you're working in, export the token environment variable:
+
+**Windows**
+
+``` shell
+$ set PERCY_TOKEN="<token>"
+
+# PowerShell
+$ $Env:PERCY_TOKEN="<token>"
+```
 
 ## Run Tests
 1. All tests: `npm test`
-1. headless: `npm run cy:headless`
+1. Headless: `npm run cy:headless`
+
+## Watch video
+Once tests were executed, video files will be created matching spec filenames under videos folder.
---
+++ cypress/integration/visual.tests.spec.js
@@ -0,0 +1,41 @@
+/// <reference types="Cypress" />
+
+const USERNAME = Cypress.env('username')
+const PASSWORD = Cypress.env('password')
+const fromAccount = Cypress.env('baseAccount')
+
+context('Verify Account Creation page visuals', () => {
+
+    beforeEach(() => {
+        cy.visit('/index.htm')
+        cy.login(USERNAME, PASSWORD)
+    })
+
+    it('Verification of new account overview page visuals', () => {               
+        cy.openNewAccount('CHECKING')
+        cy.get('#newAccountId').click()
+        cy.percySnapshot('New Account Page overview')   
+    })
+
+    it('Verification of base account overview page visuals', () => {
+        cy.contains('Accounts Overview').click()
+        cy.contains(Cypress.env('baseAccount')).click()
+        cy.percySnapshot('Base Account Page overview')
+    })
+
+    it('Verification of base account overivew page visuals after a billpay', () => {
+        const PAYEENAME = 'test'
+        let fromAccount = Cypress.env('baseAccount')
+
+        cy.doBillPay(fromAccount, Cypress.env('newAccountId'), PAYEENAME, '200.00')        
+        cy.contains('Bill Payment Complete')
+
+        cy.contains('Accounts Overview').click()        
+        cy.contains(Cypress.env('baseAccount')).click()        
+        cy.percySnapshot('Base Account Page overview after billpay')
+
+        cy.contains('Accounts Overview').click()        
+        cy.contains(Cypress.env('newAccountId')).click()        
+        cy.percySnapshot('New Account Page overview after billpay')
+    })
+})
